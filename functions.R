c3562d01afd6cb4be75158554b8291c9c4dbb758
BarnettKirk <barnett.kirk@gmail.com>
2016-02-02 13:36:39 +1100

    Added some level correction to add.factors

--- functions.R
+++ functions.R
@@ -327,11 +327,12 @@ add.factors <- function(x){
   side <- x$plot
   side<-recode(side, "c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)='Vineyard'")
   side<-recode(side, "c(31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60)='Fence'")
+  
   x$side <- side
-  x$treatment <- factor(x$treatment)
   x$side <- factor(x$side)
-  x$herb <- factor(x$herb)
-  x$treatment <- factor(x$treatment, levels = c("Ambient", "Ambient(no shelter)" ,"Increased","Drought","Pulsed drought", "Seasonal"))
+  x$herb <- factor(x$herb, levels=c("Ambient","Added"))
+  x$treatment <- factor(x$treatment, levels = c("Ambient(no shelter)", "Ambient", "Increased","Drought","Pulsed drought", "Seasonal"))
+  
   x
 }
 
