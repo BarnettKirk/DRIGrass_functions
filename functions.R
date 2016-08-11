bd3ee76716edd1d4caed57eaf81240af7f9257ae
Kirk <barnett.kirk@gmail.com>
2016-08-11 15:00:14 -0500

    Added sdellipse function

--- functions.R
+++ functions.R
@@ -527,3 +527,39 @@ rainfall_col <- c("#2b83ba","#abdda4","#5e3c99","#fdae61","#d73027")
 rainfall_herb_col <- c("#2b83ba","#2b83ba","#5e3c99","#5e3c99","#fdae61","#fdae61")
 ###########Percent difference function
 per_diff <- function(x,y){round((abs(x-y)/mean(c(x,y))*100),0)}
+
+##############SDellipse- Stolen from phonTools###############
+sdellipse <- function (points, stdev = 1.96, density = 0.01, add = TRUE, show = TRUE, 
+                       means = NULL, se = FALSE, ...) 
+{
+  if (ncol(points) != 2) 
+    stop("Points input must have exactly two columns.")
+  if (!is.null(means) & nrow(points) > 2) 
+    stop("Covariance matrix must be 2 by 2.")
+  if (!is.null(means) & length(means) > 2) 
+    stop("Exactly two means must be specified.")
+  t = seq(0, 2 * pi + density, density)
+  x = rbind(cos(t), sin(t))
+  if (is.null(means)) {
+    sigma = var(points)
+    if (se) 
+      sigma = sigma/nrow(points)
+  }
+  if (!is.null(means)) {
+    sigma = points
+    if (is.numeric(se)) 
+      sigma = sigma/se
+  }
+  A = eigen(sigma)$vectors %*% (diag(sqrt(eigen(sigma)$values)) * 
+                                  stdev)
+  points = t(colMeans(points) + A %*% x)
+  if (is.null(means)) 
+    points = t(colMeans(points) + A %*% x)
+  if (!is.null(means)) 
+    points = t(means + A %*% x)
+  if (add == TRUE & show == TRUE) 
+    lines(points, ...)
+  if (add == FALSE & show == TRUE) 
+    plot(points, type = "l", ...)
+  invisible(points)
+}
