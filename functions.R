6d9b61d1d242d03b9b155200b5d5b41ba5476633
Kirk <barnett.kirk@gmail.com>
2016-08-17 14:25:50 -0500

    commented out stupid plotresid

--- functions.R
+++ functions.R
@@ -563,44 +563,44 @@ sdellipse <- function (points, stdev = 1.96, density = 0.01, add = TRUE, show = 
     plot(points, type = "l", ...)
   invisible(points)
 }
-##########Stolen function from RVAideMemoire
-plotresid <-
-  function(model) {
-    if ("lm"%in%class(model)) {
-      if (!"glm"%in%class(model)) {
-        model.residuals<-residuals(model)
-        res.lab<-"Residuals"
-      } else {
-        if ("negbin"%in%class(model)) {
-          model.residuals<-qresiduals(model)
-          res.lab<-"Quantile residuals"
-        } else {
-          laws<-c("poisson","quasipoisson","binomial","quasibinomial")
-          if (model$family[1]%in%laws) {
-            model.residuals<-qresiduals(model)
-            res.lab<-"Quantile residuals"
-          } else {
-            model.residuals<-residuals(model)
-            res.lab<-"Residuals"
-          }
-        }
-      }
-    } else if (class(model)[1]=="mer") {
-      model.residuals<-residuals(model)
-      res.lab<-"Residuals"
-    } else {
-      stop("model not recognized")
-    }
-    fit<-fitted(model)
-    cat("Open a new window (y/n)? ")
-    resp<-as.character(readLines(n=1))
-    if (resp=="y") {
-      dev.new(height=6,width=12)
-    }
-    par(mfrow=c(1,2))
-    plot(fit,model.residuals,xlab="Fitted values",ylab=res.lab,main=paste(res.lab,"vs fitted"))
-    abline(h=0,col="grey",lty=3)
-    panel.smooth(fit,model.residuals)
-    qqnorm(model.residuals)
-    shapiro.test(model.residuals)
-  }
+# ##########Stolen function from RVAideMemoire
+# plotresid <-
+#   function(model) {
+#     if ("lm"%in%class(model)) {
+#       if (!"glm"%in%class(model)) {
+#         model.residuals<-residuals(model)
+#         res.lab<-"Residuals"
+#       } else {
+#         if ("negbin"%in%class(model)) {
+#           model.residuals<-qresiduals(model)
+#           res.lab<-"Quantile residuals"
+#         } else {
+#           laws<-c("poisson","quasipoisson","binomial","quasibinomial")
+#           if (model$family[1]%in%laws) {
+#             model.residuals<-qresiduals(model)
+#             res.lab<-"Quantile residuals"
+#           } else {
+#             model.residuals<-residuals(model)
+#             res.lab<-"Residuals"
+#           }
+#         }
+#       }
+#     } else if (class(model)[1]=="mer") {
+#       model.residuals<-residuals(model)
+#       res.lab<-"Residuals"
+#     } else {
+#       stop("model not recognized")
+#     }
+#     fit<-fitted(model)
+#     cat("Open a new window (y/n)? ")
+#     resp<-as.character(readLines(n=1))
+#     if (resp=="y") {
+#       dev.new(height=6,width=12)
+#     }
+#     par(mfrow=c(1,2))
+#     plot(fit,model.residuals,xlab="Fitted values",ylab=res.lab,main=paste(res.lab,"vs fitted"))
+#     abline(h=0,col="grey",lty=3)
+#     panel.smooth(fit,model.residuals)
+#     qqnorm(model.residuals)
+#     shapiro.test(model.residuals)
+#   }
